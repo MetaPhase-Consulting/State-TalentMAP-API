5389ea66a2b7a5997371dab4085b2af24f0ccbf9
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2020-10-02 15:11:29 -0400

    actual import

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -13,8 +13,9 @@ import requests  # pylint: disable=unused-import
 import talentmap_api.fsbid.services.common as services
 import talentmap_api.fsbid.services.cdo as cdo_services
 import talentmap_api.fsbid.services.available_positions as services_ap
+import talentmap_api.fsbid.services.employee as employee_services
 from talentmap_api.common.common_helpers import ensure_date
-from talentmap_api.fsbid.services.employee import get_employee_perdet_seq_num
+
 
 API_ROOT = settings.FSBID_API_URL
 HRDATA_URL = settings.HRDATA_URL
