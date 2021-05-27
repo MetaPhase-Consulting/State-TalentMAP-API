173be92d2bf00ce736f651e353011b4ee87f80e9
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-05-27 09:11:23 -0400

    Update bidlist.py

--- talentmap_api/fsbid/views/bidlist.py
+++ talentmap_api/fsbid/views/bidlist.py
@@ -28,8 +28,10 @@ class FSBidListView(APIView):
         Gets all bids for the current user and position information on those bids
         '''
         user = UserProfile.objects.get(user=self.request.user)
-        # user_bids = services.user_bids(user.emp_id, request.META['HTTP_JWT'])
-        return Response({"results": services.user_bids(user.emp_id, request.META['HTTP_JWT'])})
+        user_bids = services.user_bids(user.emp_id, request.META['HTTP_JWT'])
+        for bid in user_bids:
+            pos_id = bid["position"]["id"]
+        return Response({"results": user_bids})
 
 
 class FSBidBidListCSVView(APIView):
