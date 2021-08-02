cf469b83971c9437410ed3e479d2c3fe7a761821
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-08-02 17:41:15 -0400

    non-cdo classifications set up

---
+++ talentmap_api/fsbid/views/classifications.py
@@ -0,0 +1,21 @@
+import coreapi
+
+from rest_framework.response import Response
+from rest_framework.schemas import AutoSchema
+
+from talentmap_api.fsbid.views.base import BaseView
+import talentmap_api.fsbid.services.classifications as classifications_services
+
+
+class FSBidClassificationsView(BaseView):
+    schema = AutoSchema(
+        manual_fields=[
+            coreapi.Field("perdet_seq_num", location='query', description='ID of user'),
+        ]
+    )
+
+    def get(self, request, pk):
+        '''
+        Get user's classifications
+        '''
+        return Response(classifications_services.get_user_classifications(request.META['HTTP_JWT'], pk))
