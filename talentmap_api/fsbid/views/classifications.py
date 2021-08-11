5e704bcd0fc80366c3027ad750bcb2417ff36884
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2021-08-11 16:15:30 -0400

    whoops

--- talentmap_api/fsbid/views/classifications.py
+++ talentmap_api/fsbid/views/classifications.py
@@ -2,6 +2,7 @@ import coreapi
 
 from rest_framework.response import Response
 from rest_framework.schemas import AutoSchema
+from rest_framework.permissions import IsAuthenticated
 
 from talentmap_api.fsbid.views.base import BaseView
 import talentmap_api.fsbid.services.classifications as classifications_services
