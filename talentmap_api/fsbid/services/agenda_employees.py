4450d30495ca5c01b3217894e53e20407c86580f
Mike Joyce <joyce_michael@bah.com>
2022-02-18 14:58:35 -0500

    Integrate with tm-employees endpoint

--- talentmap_api/fsbid/services/agenda_employees.py
+++ talentmap_api/fsbid/services/agenda_employees.py
@@ -2,6 +2,7 @@ import logging
 import jwt
 import pydash
 import re
+import maya
 from urllib.parse import urlencode, quote
 
 from django.conf import settings
@@ -19,15 +20,14 @@ def get_agenda_employees(query, jwt_token=None, host=None):
     Get employees
     '''
     args = {
-        "uri": "agendaItems",
+        "uri": "v1/tm-persons",
         "query": query,
         "query_mapping_function": convert_agenda_employees_query,
         "jwt_token": jwt_token,
         "mapping_function": fsbid_agenda_employee_to_talentmap_agenda_employee,
-        "count_function": False,
+        "count_function": get_agenda_employees_count,
         "base_url": '',
         "host": host,
-        "api_root": PERSON_API_ROOT,
         "use_post": False,
     }
 
@@ -37,47 +37,78 @@ def get_agenda_employees(query, jwt_token=None, host=None):
 
     return agenda_employees
 
-# TO-DO
-# def get_agenda_employees_count(query, jwt_token, host=None, use_post=False):
-#     '''
-#     Get total number of employees for agenda search
-#     '''
-#     args = {
-#         "uri": "",
-#         "query": query,
-#         "query_mapping_function": convert_agenda_employees_query,
-#         "jwt_token": jwt_token,
-#         "host": host,
-#         "api_root": PERSON_API_ROOT,
-#         "use_post": False,
-#     }
-#     return services.send_count_request(**args)
+
+def get_agenda_employees_count(query, jwt_token, host=None, use_post=False):
+    '''
+    Get total number of employees for agenda search
+    '''
+    args = {
+        "uri": "v1/tm-persons",
+        "query": query,
+        "query_mapping_function": convert_agenda_employees_query,
+        "jwt_token": jwt_token,
+        "host": host,
+        "use_post": False,
+        "is_template": True,
+    }
+    return services.send_count_request(**args)
 
 def convert_agenda_employees_query(query):
     '''
     Convert TalentMAP filters into FSBid filters
     '''
-    filterValue = query.get("q", None)
-    filterKey = ''
-    comparator = 'eq'
-    if filterValue:
+    qFilterValue = query.get("q", None)
+    qFilterKey = ''
+    qComparator = 'eq'
+    if qFilterValue:
         # employee IDs can contain letters, and names can contain numbers. This does a best guess at the user's intent
-        if len(''.join(re.findall('[0-9]+', filterValue))) > 2:
-            filterKey = 'pertexternalid'
+        if len(''.join(re.findall('[0-9]+', qFilterValue))) > 2:
+            qFilterKey = 'pertexternalid'
         else:
-            filterKey = 'perpiifullname'
-            comparator = 'contains'
-            filterValue = filterValue.upper()
+            qFilterKey = 'perpiifullname'
+            qComparator = 'contains'
+            qFilterValue = qFilterValue.upper()
+    
+    tedStart = query.get("ted-start")
+    tedEnd = query.get("ted-end")
+    
+    filters = [
+        { "key": "tmpercurrentbureaucode", "comparator": "IN", "value": query.get("current-bureaus", None) },
+        { "key": "tmperhsbureaucode", "comparator": "IN", "value": query.get("handshake-bureaus", None) },
+        { "key": "tmpercurrentorgcode", "comparator": "IN", "value": query.get("current-organizations", None) },
+        { "key": "tmperhsorgcode", "comparator": "IN", "value": query.get("handshake-organizations", None) },
+        { "key": "tmpercdoid", "comparator": "IN", "value": query.get("cdos", None) },
+        { "key": "tmperhsind", "comparator": "IN", "value": query.get("handshake", None) },
+    ]
+
+    try:
+        if tedStart and tedEnd:
+            startVal = maya.parse(tedStart).datetime().strftime("%Y-%m-%d")
+            endVal = maya.parse(tedEnd).datetime().strftime("%Y-%m-%d")
+            filters.append({ "key": "tmpercurrentted", "comparator": "GTEQ", "value": startVal, "isDate": True })
+            filters.append({ "key": "tmpercurrentted", "comparator": "LTEQ", "value": endVal, "isDate": True })
+    except:
+        logger.info(f"Invalid date {tedStart} or {tedEnd} could not be parsed.")
+
+    filters = pydash.filter_(filters, lambda o: o["value"] != None)
+
+    filters = pydash.map_(filters, lambda x: services.convert_to_fsbid_ql(x["key"], x["value"], x["comparator"], pydash.get(x, "isDate")))
+
+    if qFilterKey and qFilterValue:
+        filters.append(services.convert_to_fsbid_ql(qFilterKey, qFilterValue, qComparator))
 
     values = {
         # Pagination
         "rp.pageNum": query.get("page", 1),
         "rp.pageRows": query.get("limit", 50),
         "rp.orderBy": services.sorting_values(query.get("ordering", "agenda_employee_fullname")),
-        "rp.filter": services.convert_to_fsbid_ql(filterKey, filterValue, comparator),
-        # services.convert_to_fsbid_ql('perdetseqnum', query.get("q", None)),
-        # services.convert_to_fsbid_ql('perpiilastname', query.get("q", None)), TODO - passing multiples values
+        "rp.filter": filters,
     }
+    if query.get("getCount") == 'true':
+        values["rp.pageNum"] = 0
+        values["rp.pageRows"] = 0
+        values["rp.columns"] = "ROWCOUNT"
+
     valuesToReturn = pydash.omit_by(values, lambda o: o is None or o == [])
     return urlencode(valuesToReturn, doseq=True, quote_via=quote)
 
@@ -85,52 +116,59 @@ def fsbid_agenda_employee_to_talentmap_agenda_employee(data):
     '''
     Maps FSBid response to expected TalentMAP response
     '''
-    current = data.get("currentAssignment", [])
-    currentAssignment = pydash.get(current, '[0]') or {} if current else {}
-    pos = pydash.get(currentAssignment, "position") or []
-    position = pydash.get(pos, '[0]') or {} if pos else {}
-    firstN = data.get('perpiifirstname', '')
-    lastN = data.get('perpiilastname', '')
-    initials = f"{firstN[0] if firstN else ''}{lastN[0] if lastN else ''}"
-    fullName = data.get("perpiifullname", "")
+    fullName = data.get("tmperperfullname", "")
     if pydash.ends_with(fullName, "NMN"):
         fullName = fullName.rstrip(" NMN")
     if pydash.ends_with(fullName, "Nmn"):
         fullName = fullName.rstrip(" Nmn")
     return {
         "person": {
-            "lastName": lastN,
-            "firstName": firstN,
-            "middleName": data.get("perpiimiddlename", ""),
-            "suffix": data.get("perpiisuffixname", ""),
             "fullName": fullName,
-            "perdet": data.get("perdetseqnum", ""),
-            "employeeID": data.get("pertexternalid", ""),
-            "employeeSeqNumber": data.get("perpiiseqnum", ""),
-            "orgCode": data.get("perdetorgcode", ""),
-            "initials": initials,
-            # data.get("perdetperscode", ""),
-            # data.get("pertexternalid", ""),
-            # data.get("perdetorgcode", ""),
-            # data.get("pertcurrentind", ""),
-            # data.get("perdetminactemplrcd#ind", ""),
-            # data.get("persdesc", ""),
-            # data.get("rnum", ""),
+            "perdet": data.get("tmperperdetseqnum", ""),
+            "employeeID": data.get("tmperpertexternalid", ""),
+            "initials": "",
         },
         "currentAssignment": {
-            "TED": currentAssignment.get("asgdetdteddate", ""),
-            "TOD": currentAssignment.get("asgdtodcode", ""),
-            "positionSequenceNumber": position.get("posseqnum", ""),
-            "orgDescription": position.get("posorgshortdesc", ""),
-            "positionNumber": position.get("posnumtext", ""),
-            "grade": position.get("posgradecode", ""),
-            "positionTitle": position.get("postitledesc", ""),
-            # currentAssignment.get("asgperdetseqnum", ""),
-            # currentAssignment.get("asgempseqnbr", ""),
-            # currentAssignment.get("asgposseqnum", ""),
-            # currentAssignment.get("asgdasgseqnum", ""),
-            # currentAssignment.get("asgdrevisionnum", ""),
-            # currentAssignment.get("asgdasgscode", ""),
-            # currentAssignment.get("latestAgendaItem", []),
+            "TED": data.get("tmpercurrentted", ""),
+            "orgDescription": data.get("tmpercurrentorgdesc", ""),
+        },
+        "hsAssignment": {
+            "orgDescription": data.get("tmperhsorgdesc", ""),
+        },
+        "agenda": {
+            "panelDate": data.get("tmperpanelmeetingdate", ""),
+            "status": data.get("tmperaiscode", ""),
         }
+    }
+
+
+@staticmethod
+def fsbid_person_current_organization_to_talentmap(data):
+    return {
+        "code": data.get("tmpercurrentorgcode", None),
+        "name": data.get("tmpercurrentorgdesc", None),
+    }
+
+
+@staticmethod
+def fsbid_person_handshake_organization_to_talentmap(data):
+    return {
+        "code": data.get("tmperhsorgcode", None),
+        "name": data.get("tmperhsorgdesc", None),
+    }
+
+
+@staticmethod
+def fsbid_person_current_bureau_to_talentmap(data):
+    return {
+        "code": data.get("tmpercurrentbureaucode", None),
+        "name": data.get("tmpercurrentbureaudesc", None),
+    }
+
+
+@staticmethod
+def fsbid_person_handshake_bureau_to_talentmap(data):
+    return {
+        "code": data.get("tmperhsbureaucode", None),
+        "name": data.get("tmperhsbureaudesc", None),
     }
