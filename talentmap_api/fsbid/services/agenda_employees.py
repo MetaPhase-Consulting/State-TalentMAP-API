d86de11bb67948bba60f7905b2d8b73b0e0894df
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2023-04-14 16:51:34 -0400

    TM-4265, add suffix to tm persons return name

--- talentmap_api/fsbid/services/agenda_employees.py
+++ talentmap_api/fsbid/services/agenda_employees.py
@@ -50,6 +50,55 @@ def get_agenda_employees(query, jwt_token=None, host=None):
 
     return agenda_employees
 
+def get_v3_persons(pk, jwt_token=None, host=None):
+    '''
+    Get an employee by perdetseqnum from v3/persons
+    '''
+    args = {
+        "uri": "",
+        "query": {'perdetseqnum': pk},
+        "query_mapping_function": convert_v3_persons_query,
+        "jwt_token": jwt_token,
+        'mapping_function': v3_persons_to_tm,
+        "count_function": None,
+        "base_url": '',
+        "host": host,
+        "api_root": PERSON_API_ROOT,
+        "use_post": False,
+    }
+
+    agenda_employees = services.send_get_request(
+        **args
+    )
+
+    return agenda_employees
+
+def convert_v3_persons_query(query):
+    '''
+    Converts TalentMap filters into FSBid filters
+    '''
+    values = {
+        "rp.pageNum": int(query.get("page", 1)),
+        "rp.pageRows": int(query.get("limit", 1)),
+        "rp.columns": None,
+        "rp.filter": services.convert_to_fsbid_ql([
+            {'col': 'perdetseqnum', 'val': query.get("perdetseqnum", None)},
+        ]),
+    }
+
+    valuesToReturn = pydash.omit_by(values, lambda o: o is None or o == [])
+
+    return urlencode(valuesToReturn, doseq=True, quote_via=quote)
+
+def v3_persons_to_tm(data):
+    firstN = data.get('perpiifirstname', '')
+    lastN = data.get('perpiilastname', '')
+    name = firstN +' '+ lastN
+
+    return {
+      'name': name,
+      'perdet_seq_number': str(data.get('perdetseqnum', '')),
+    }
 
 def get_agenda_employees_count(query, jwt_token, host=None, use_post=False):
     '''
@@ -210,6 +259,10 @@ def fsbid_agenda_employee_to_talentmap_agenda_employee(data, cdos=[]):
     firstN = data.get('perpiifirstname', '')
     lastN = data.get('perpiilastname', '')
     initials = f"{firstN[0] if firstN else ''}{lastN[0] if lastN else ''}"
+    suffix = data.get('perpiisuffixname', '')
+    hasSuffix = len(suffix) > 0
+    if suffix == " ":
+        hasSuffix = False
     fullName = data.get("perpiifullname", "")
     if pydash.ends_with(fullName, "NMN"):
         fullName = fullName.rstrip(" NMN")
@@ -222,7 +275,7 @@ def fsbid_agenda_employee_to_talentmap_agenda_employee(data, cdos=[]):
         cdo = cdoObj
     return {
         "person": {
-            "fullName": fullName,
+            "fullName": fullName + f"{', '+suffix if hasSuffix else ''} ",
             "perdet": data.get("perdetseqnum", ""),
             "employeeID": data.get("pertexternalid", ""),
             "initials": initials,
