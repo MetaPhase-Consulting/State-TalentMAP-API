fdd059b944ae62495099e1d3304e12d2ef5c6b57
pveland621 <31417027+pveland621@users.noreply.github.com>
2023-09-11 14:02:52 -0400

    initial dev1 test

---
+++ talentmap_api/fsbid/urls/job_categories.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from rest_framework import routers
+
+from talentmap_api.fsbid.views import job_categories as views
+
+router = routers.SimpleRouter()
+urlpatterns = [
+    # url(r'^filters/$', views.FSBidPostAccessFiltersView.as_view(), name='FSBid-post-access-filters'),
+    url(r'^$', views.FSBidJobCategoriesListView.as_view(), name='FSBid-job-categories-list'),
+    url(r'^skills/$', views.FSBidJobCategorySkillsListView.as_view(), name='FSBid-job-category-skills-list'),
+    # url(r'^permissions/$', views.FSBidPostAccessActionView.as_view(), name='FSBid-post-access-action'),
+]
+
+urlpatterns += router.urls
