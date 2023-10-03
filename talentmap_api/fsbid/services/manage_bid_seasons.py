845fa5040d2d924f978e51a2f4e09298f2029a69
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2023-10-03 19:22:10 -0400

    update bid seasons req res

--- talentmap_api/fsbid/services/manage_bid_seasons.py
+++ talentmap_api/fsbid/services/manage_bid_seasons.py
@@ -36,7 +36,12 @@ def fsbid_to_tm_mbs_data_mapping(data):
             'bid_seasons_begin_date': x.get('BSN_START_DATE') or '---',
             'bid_seasons_end_date': x.get('BSN_END_DATE') or '---',
             'bid_seasons_panel_cutoff': x.get('BSN_PANEL_CUTOFF_DATE') or '---',
-            'bid_seasons_future_vacancy': x.get('BSN_FUTURE_VACANCY_IND') or '---',
+            'bid_seasons_future_vacancy': x.get('BSN_FUTURE_VACANCY_IND') or 'N',
+            'bid_seasons_snt_seq_num': x.get('SNT_SEQ_NUM') or '---',
+            'bid_seasons_create_id': x.get('BSN_CREATE_ID') or '---',
+            'bid_seasons_create_date': x.get('BSN_CREATE_DATE') or '---',
+            'bid_seasons_update_id': x.get('BSN_UPDATE_ID') or '---',
+            'bid_seasons_update_date': x.get('BSN_UPDATE_DATE') or '---',
         }
     return list(map(bsm_results_mapping, data.get('PQRY_CUST_BSN_TAB_O')))
 
@@ -50,7 +55,7 @@ def update_bid_seasons_data(jwt_token, request):
     "proc_name": 'prc_iud_bid_season',
     "package_name": 'PKG_WEBAPI_WRAP_SPRINT98',
     "request_body": request,
-    "request_mapping_function": map_bid_season_post_request,
+    "request_mapping_function": bid_seasons_req_mapping,
     "response_mapping_function": bid_seasons_res_mapping,
     "jwt_token": jwt_token,
     }
@@ -59,28 +64,39 @@ def update_bid_seasons_data(jwt_token, request):
         **args
     )
 
+
 def bid_seasons_res_mapping(data):
     if data is None or (data['PV_RETURN_CODE_O'] and data['PV_RETURN_CODE_O'] is not 0):
         logger.error(f"Fsbid call to Bid Seasons failed.")
         return None
 
-def map_bid_season_post_request(req):
+
+def bid_seasons_req_mapping(req):
+    isUpdate = True if id in req['data'] else False # Insert will not pass an ID
+
     mapped_request = {
       "PV_API_VERSION_I": "",
       "PV_AD_ID_I": "",
-      "PV_ACTION_I": "U" if req['data']['id'] else "I",
+      "PV_ACTION_I": "U" if isUpdate else "I",
     }
 
-    mapped_request['PTYP_CUST_BSN_TAB_I'] = format_request_post_data_to_string(req)
+    mapped_request['PTYP_CUST_BSN_TAB_I'] = format_request_post_data_to_string(req, isUpdate)
     return mapped_request
 
-def format_request_post_data_to_string(request_values):
-    id = request_values['data']['id'] or ''
-    name = request_values['data']['name']
-    start_date = request_values['data']['startDate'][:10]
-    end_date = request_values['data']['endDate'][:10]
-    panel_cutoff_date = request_values['data']['panelCutoffDate'][:10]
-    future_vacancy = request_values['data']['futureVacancy']
+
+def format_request_post_data_to_string(request, isUpdate):
+    id = request['data']['id'] if isUpdate else ''
+    name = request['data']['name']
+    start_date = request['data']['startDate'][:10]
+    end_date = request['data']['endDate'][:10]
+    panel_cutoff_date = request['data']['panelCutoffDate'][:10]
+    future_vacancy = request['data']['futureVacancy']
+    snt_seq_num = request['data']['bid_seasons_snt_seq_num']
+    # below values are required for UPDATE, but not for INSERT
+    create_id = request['data']['bid_seasons_create_id'] if isUpdate else ""
+    create_date = request['data']['bid_seasons_create_date'][:10] if isUpdate else ""
+    update_id = request['data']['bid_seasons_update_id'] if isUpdate else ""
+    update_date = request['data']['bid_seasons_update_date'][:10] if isUpdate else ""
 
     new_dict = {
       "Data": [{
@@ -88,13 +104,13 @@ def format_request_post_data_to_string(request_values):
           "BSN_DESCR_TEXT": name,
           "BSN_START_DATE": start_date,
           "BSN_END_DATE": end_date,
-          "BSN_CREATE_ID": '',
-          "BSN_CREATE_DATE": '',
-          "BSN_UPDATE_ID": '',
-          "BSN_UPDATE_DATE": '',
           "BSN_PANEL_CUTOFF_DATE": panel_cutoff_date,
           "BSN_FUTURE_VACANCY_IND": future_vacancy,
-          "SNT_SEQ_NUM": ''
+          "SNT_SEQ_NUM": snt_seq_num,
+          "BSN_CREATE_ID": create_id,
+          "BSN_CREATE_DATE": create_date,
+          "BSN_UPDATE_ID": update_id,
+          "BSN_UPDATE_DATE": update_date,
       }]
     }
     return new_dict
