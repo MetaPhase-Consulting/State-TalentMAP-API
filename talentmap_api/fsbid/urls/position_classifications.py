568158bc311e9c9e13b8fab0d2ff21c189395029
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-10-04 09:23:27 -0400

    Added position classifications urls

---
+++ talentmap_api/fsbid/urls/position_classifications.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from rest_framework import routers
+
+from talentmap_api.fsbid.views import position_classifications as views
+
+router = routers.SimpleRouter()
+
+urlpatterns = [
+    url(r'^$', views.FSBidPositionClassificationsView.as_view(), name='FSBid-position-classifications'),
+    url(r'^edit/$', views.FSBidPositionClassificationsActionView.as_view(), name='FSBid-position-classifications-edit'),
+]
+
+urlpatterns += router.urls
