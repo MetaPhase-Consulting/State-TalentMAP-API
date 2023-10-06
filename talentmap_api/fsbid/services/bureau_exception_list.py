5091584a8e8bdace2be47e7cb2e371a25d05cc76
musiqe31 <agbakwucharles@gmail.com>
2023-10-06 09:04:16 -0500

    refactored endpoints

--- talentmap_api/fsbid/services/bureau_exception_list.py
+++ talentmap_api/fsbid/services/bureau_exception_list.py
@@ -11,58 +11,6 @@ BUREAU_EXCEPTION_LIST_ROOT = settings.BUREAU_EXCEPTION_LIST_API_URL
 
 logger = logging.getLogger(__name__)
 
-def get_capsule_description(id, jwt_token):
-    '''
-    Gets an individual capsule description
-    '''
-    capsule_description = services.send_get_request(
-        "capsule",
-        {"id": id},
-        convert_capsule_query,
-        jwt_token,
-        fsbid_capsule_to_talentmap_capsule,
-        None,
-        "/api/v1/fsbid/bureauExceptionList/",
-        None,
-        BUREAU_EXCEPTION_LIST_ROOT
-    )
-    return pydash.get(capsule_description, 'results[0]') or None
-
-def update_capsule_description(jwt_token, id, description, last_updated_date, updater_id):
-    '''
-    Updates capsule description on bureau exception list
-    '''
-    ad_id = jwt.decode(jwt_token, verify=False).get('unique_name')
-    uri = f"{BUREAU_EXCEPTION_LIST_ROOT}/capsule"
-    url = f"{uri}?pos_seq_num={id}&capsule_descr_txt={description}&update_id={last_updated_date}&update_id={updater_id}&ad_id={ad_id}"
-    response = requests.patch(url, data={}, headers={'JWTAuthorization': jwt_token, 'Content-Type': 'application/json'})
-    response.raise_for_status()
-    return response
-
-def fsbid_capsule_to_talentmap_capsule(capsule):
-    '''
-    Formats FSBid response to Talentmap format
-    '''
-    return {
-        "id": capsule.get("pos_seq_num", None),
-        "description": capsule.get("capsule_descr_txt", None),
-        "last_updated_date": capsule.get("update_date", None),
-        "updater_id": capsule.get("update_id", None),
-    }
-
-def convert_capsule_query(query):
-    '''
-    Converts TalentMap query to FSBid
-    '''
-    values = {
-        "pos_seq_num": query.get("id", None),
-        "ad_id": query.get("ad_id", None),
-        "update_date": query.get("last_updated_date", None),
-        "update_id": query.get("updater_id", None),
-        "capsule_descr_txt": query.get("description", None),
-    }
-    return urlencode({i: j for i, j in values.items() if j is not None}, doseq=True, quote_via=quote)
-
 def get_bureau_exception_list(query, jwt_token):
     '''
     Gets Bureau Exception List
