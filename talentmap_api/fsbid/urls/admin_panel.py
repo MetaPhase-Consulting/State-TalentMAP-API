c4117614d2181ab3493ce15eae0299d6c5686fc7
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-10-16 19:08:14 -0400

    Added new panel meeting endpoints

--- talentmap_api/fsbid/urls/admin_panel.py
+++ talentmap_api/fsbid/urls/admin_panel.py
@@ -7,6 +7,13 @@ router = routers.SimpleRouter()
 
 urlpatterns = [
     url(r'^remark/$', views.CreateRemarkView.as_view(), name='administration-createremark'),
+    url(r'^(?P<pk>[0-9]+)/$', views.FSBidPanelMeetingView.as_view(), name='FSBID-panel-meeting'),
+    url(r'^edit/$', views.FSBidPanelMeetingActionView.as_view(), name='FSBID-panel-meeting-actions'),
+    url(r'^post_panel/(?P<pk>[0-9]+)/$', views.FSBidPostPanelView.as_view(), name='FSBid-post-panel-processing'),
+    url(r'^post_panel/edit/$', views.FSBidPostPanelActionView.as_view(), name='FSBid-post-panel-processing-actions'),
+    url(r'^run/preliminary/(?P<pk>[0-9]+)/$', views.FSBidRunPreliminaryActionView.as_view(), name='FSBID-run-preliminary-action'),
+    url(r'^run/addendum/(?P<pk>[0-9]+)/$', views.FSBidRunAddendumActionView.as_view(), name='FSBID-run-addendum-action'),
+    url(r'^run/post_panel/(?P<pk>[0-9]+)/$', views.FSBidRunPostPanelActionView.as_view(), name='FSBID-run-post-panel-action'),
 ]
 
 urlpatterns += router.urls
