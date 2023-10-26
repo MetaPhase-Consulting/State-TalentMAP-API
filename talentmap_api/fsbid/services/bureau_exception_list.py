fa7605d2a13e1f7c49e083ded67a088bd0b1d710
musiqe31 <agbakwucharles@gmail.com>
2023-10-26 14:27:43 -0500

    updated response data

--- talentmap_api/fsbid/services/bureau_exception_list.py
+++ talentmap_api/fsbid/services/bureau_exception_list.py
@@ -121,10 +121,6 @@ def bureau_exception_list_res_mapping(data):
         return None
         
     def bureau_execp_map(x):
-        if x.get('PARM_VALUES') == None:
-            print('None Type Here')
-        else:
-            print(x.get('PARM_VALUES').split(","))
         return {
             'pv_id': x.get('PV_ID') or '-',
             'name': x.get('EMP_FULL_NAME'),
