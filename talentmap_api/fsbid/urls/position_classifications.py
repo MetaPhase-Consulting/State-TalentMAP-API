2abad6349ef392cb741b0e397f1749e7e96b2a3e
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-11-07 15:14:55 -0500

    Fixed regex to account for alphanumeric prefix

--- talentmap_api/fsbid/urls/position_classifications.py
+++ talentmap_api/fsbid/urls/position_classifications.py
@@ -6,7 +6,7 @@ from talentmap_api.fsbid.views import position_classifications as views
 router = routers.SimpleRouter()
 
 urlpatterns = [
-    url(r'^(?P<pk>[0-9]+)/$', views.FSBidPositionClassificationsView.as_view(), name='FSBid-position-classifications'),
+    url(r'^(?P<pk>\w{2,3}[0-9]+)/$', views.FSBidPositionClassificationsView.as_view(), name='FSBid-position-classifications'),
     url(r'^edit/$', views.FSBidPositionClassificationsActionView.as_view(), name='FSBid-position-classifications-edit'),
 ]
 
