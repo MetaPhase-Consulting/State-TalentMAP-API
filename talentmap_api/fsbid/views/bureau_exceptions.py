685f5d02f277c36d054cc9eed7eda5e31b4fbefb
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-11-21 11:09:47 -0500

    updated error handling

--- talentmap_api/fsbid/views/bureau_exceptions.py
+++ talentmap_api/fsbid/views/bureau_exceptions.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 
 import talentmap_api.fsbid.services.bureau_exceptions as services
 from talentmap_api.common.permissions import isDjangoGroupMember
+from django.core.exceptions import ValidationError
 
 
 class FSBidBureauExceptionsView(APIView):
@@ -65,8 +66,11 @@ class FSBidBureauExceptionsUserAddActionView(APIView):
         Add Bureau Exceptions to a User
         used the first time Bureau Exceptions are added to a user
         '''
-        result = services.add_user_bureau_exceptions(request.data, request.META['HTTP_JWT'])
-        if result is None:
+        try:
+            services.add_user_bureau_exceptions(request.data, request.META['HTTP_JWT'])
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -89,10 +93,13 @@ class FSBidBureauExceptionsUserUpdateActionView(APIView):
         '''
         Update User Bureau Exceptions
         '''
-        results = services.update_user_bureau_exceptions(request.data, request.META['HTTP_JWT'])
-        if results is None:
+        try:
+            services.update_user_bureau_exceptions(request.data, request.META['HTTP_JWT'])
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        
+
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class FSBidBureauExceptionsUserDeleteActionView(APIView):
@@ -114,8 +121,11 @@ class FSBidBureauExceptionsUserDeleteActionView(APIView):
         Deletes all Bureau Exceptions from a User
         will remove all Bureau Exceptions from User, regardless of it all Bureaus are sent in for removal
         '''
-        results = services.delete_user_bureau_exceptions(request.data, request.META['HTTP_JWT'])
-        if results is None:
+        try:
+            services.delete_user_bureau_exceptions(request.data, request.META['HTTP_JWT'])
+        except ValidationError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        
+
         return Response(status=status.HTTP_204_NO_CONTENT)
