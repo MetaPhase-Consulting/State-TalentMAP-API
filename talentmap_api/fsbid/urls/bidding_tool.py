fa5b1c4ba6ae80c7732d201276f56fb1a22eb9dc
Jeanie Lam <jlam@metaphaseconsulting.com>
2023-12-19 09:15:46 -0500

    Added get create data URL

--- talentmap_api/fsbid/urls/bidding_tool.py
+++ talentmap_api/fsbid/urls/bidding_tool.py
@@ -8,6 +8,7 @@ router = routers.SimpleRouter()
 urlpatterns = [
     url(r'^(?P<pk>\w{2}[0-9]+)/$', views.FSBidBiddingToolView.as_view(), name='FSBid-bidding-tool'),
     url(r'^$', views.FSBidBiddingToolActionsView.as_view(), name='FSBid-bidding-tool-actions'),
+    url(r'^create/$', views.FSBidBiddingToolCreateView.as_view(), name='FSBid-bidding-tool-create'),
 ]
 
 urlpatterns += router.urls
