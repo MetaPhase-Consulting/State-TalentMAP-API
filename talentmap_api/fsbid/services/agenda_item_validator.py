cb5683cbbe0d542505726af1290fa1f73beb7986
Mostafa Habib <mostafahabib@mpc-l7h4w76lm9.mynetworksettings.com>
2024-01-22 13:54:34 -0500

    separations will now have a TED

--- talentmap_api/fsbid/services/agenda_item_validator.py
+++ talentmap_api/fsbid/services/agenda_item_validator.py
@@ -124,8 +124,8 @@ def validate_individual_leg(leg):
         individual_leg_validation['eta']['errorMessage'] = 'Missing ETA'
         whole_leg_valid = False
 
-    # Leg - must have TED, if the TOD is not indefinite or N/A, or if not separation
-    if not leg['ted'] and not leg.get('is_separation') and not leg['tod'] == 'Y' and not leg['tod'] == 'Z' or False:
+    # Leg - must have TED, unless if the TOD is indefinite or N/A
+    if not leg['ted'] and not leg['tod'] == 'Y' and not leg['tod'] == 'Z' or False:
         individual_leg_validation['ted']['valid'] = False
         individual_leg_validation['ted']['errorMessage'] = 'Missing TED'
         whole_leg_valid = False
