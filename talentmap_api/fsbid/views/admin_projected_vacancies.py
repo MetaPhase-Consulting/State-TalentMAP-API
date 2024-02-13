ce965243a15803bd5500eba88097def7db9fe8a3
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-13 00:35:26 -0500

    Reorganize code

--- talentmap_api/fsbid/views/admin_projected_vacancies.py
+++ talentmap_api/fsbid/views/admin_projected_vacancies.py
@@ -20,6 +20,8 @@ logger = logging.getLogger(__name__)
 
 class FSBidAdminProjectedVacancyFiltersView(BaseView):
 
+    # ======================== Get PV Filters ========================
+
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get(self, request):
@@ -31,8 +33,36 @@ class FSBidAdminProjectedVacancyFiltersView(BaseView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(result)
 
+class FSBidAdminProjectedVacancyListView(BaseView):
+
+    # ======================== Get PV List ========================
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'bureaus': openapi.Schema(type=openapi.TYPE_STRING, description='bureaus'),
+            'organizations': openapi.Schema(type=openapi.TYPE_STRING, description='organizations'),
+            'bidSeasons': openapi.Schema(type=openapi.TYPE_STRING, description='bid_seasons'),
+            'grades': openapi.Schema(type=openapi.TYPE_STRING, description='grades'),
+            'skills': openapi.Schema(type=openapi.TYPE_STRING, description='skills'),
+            'languages': openapi.Schema(type=openapi.TYPE_STRING, description='languages'),
+        }
+    ))
+
+    def get(self, request):
+        '''
+        Gets admin projected vacancies
+        '''
+        result = services.get_admin_projected_vacancies(request.query_params, request.META['HTTP_JWT'])
+        if result is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(result)
 
 class FSBidAdminProjectedVacancyLanguageOffsetsView(BaseView):
+
+    # ======================== Get Language Offsets Dropdowns ========================
 
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
@@ -44,20 +74,3 @@ class FSBidAdminProjectedVacancyLanguageOffsetsView(BaseView):
         if result is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(result)
-
-
-
-class FSBidAdminProjectedVacancyListView(BaseView):
-
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    def get(self, request):
-        '''
-        Gets admin projected vacancies
-        '''
-        result = services.get_admin_projected_vacancies(request.query_params, request.META['HTTP_JWT'])
-        if result is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        return Response(result)
-
-
