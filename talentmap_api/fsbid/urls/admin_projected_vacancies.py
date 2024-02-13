0f9ec34b411133bc2930ce0d256d2a7235837e44
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-13 15:01:06 -0500

    Added main edit action URL

--- talentmap_api/fsbid/urls/admin_projected_vacancies.py
+++ talentmap_api/fsbid/urls/admin_projected_vacancies.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^filters/$', views.FSBidAdminProjectedVacancyFiltersView.as_view(), name="admin-projected-vacancies-filters"),
     url(r'^language_offsets/$', views.FSBidAdminProjectedVacancyLanguageOffsetsView.as_view(), name="admin-projected-vacancies-lang-offsets"),
     url(r'^$', views.FSBidAdminProjectedVacancyListView.as_view(), name="admin-projected-vacancies"),
+    url(r'^edit/$', views.FSBidAdminProjectedVacancyActionsView.as_view(), name='admin-projected-vacancies-actions'),
 ]
 
 urlpatterns += router.urls
