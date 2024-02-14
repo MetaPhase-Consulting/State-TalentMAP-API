65aea0cf5abda34d493d39273b4482c5678cae10
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-13 22:23:01 -0500

    Debug api error

--- talentmap_api/fsbid/views/admin_projected_vacancies.py
+++ talentmap_api/fsbid/views/admin_projected_vacancies.py
@@ -31,33 +31,6 @@ class FSBidAdminProjectedVacancyFiltersView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(result)
 
-class FSBidAdminProjectedVacancyListView(APIView):
-
-    # ======================== Get PV List ========================
-
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'bureaus': openapi.Schema(type=openapi.TYPE_STRING, description='bureaus'),
-            'organizations': openapi.Schema(type=openapi.TYPE_STRING, description='organizations'),
-            'bidSeasons': openapi.Schema(type=openapi.TYPE_STRING, description='bid_seasons'),
-            'grades': openapi.Schema(type=openapi.TYPE_STRING, description='grades'),
-            'skills': openapi.Schema(type=openapi.TYPE_STRING, description='skills'),
-            'languages': openapi.Schema(type=openapi.TYPE_STRING, description='languages'),
-        }
-    ))
-
-    def get(self, request):
-        '''
-        Gets List Data for Admin Projected Vacancies 
-        '''
-        result = services.get_admin_projected_vacancies(request.query_params, request.META['HTTP_JWT'])
-        if result is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        return Response(result)
-
 class FSBidAdminProjectedVacancyLanguageOffsetsView(APIView):
 
     # ======================== Get Language Offsets Dropdowns ========================
@@ -69,6 +42,21 @@ class FSBidAdminProjectedVacancyLanguageOffsetsView(APIView):
         Gets Language Offsets for Admin Projected Vacancies
         '''
         result = services.get_admin_projected_vacancy_language_offsets(request.META['HTTP_JWT'])
+        if result is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(result)
+
+class FSBidAdminProjectedVacancyListView(APIView):
+
+    # ======================== Get PV List ========================
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get(self, request):
+        '''
+        Gets List Data for Admin Projected Vacancies 
+        '''
+        result = services.get_admin_projected_vacancies(request.data, request.META['HTTP_JWT'])
         if result is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         return Response(result)
