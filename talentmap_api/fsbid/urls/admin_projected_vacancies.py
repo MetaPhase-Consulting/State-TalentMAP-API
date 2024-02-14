b92ef4dcd125cab648a031cd6c9f9e446e20f64f
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-14 08:41:18 -0500

    Added urls for edit language offsets and capsule description

--- talentmap_api/fsbid/urls/admin_projected_vacancies.py
+++ talentmap_api/fsbid/urls/admin_projected_vacancies.py
@@ -10,6 +10,8 @@ urlpatterns = [
     url(r'^language_offsets/$', views.FSBidAdminProjectedVacancyLanguageOffsetsView.as_view(), name="admin-projected-vacancies-lang-offsets"),
     url(r'^$', views.FSBidAdminProjectedVacancyListView.as_view(), name="admin-projected-vacancies"),
     url(r'^edit/$', views.FSBidAdminProjectedVacancyActionsView.as_view(), name='admin-projected-vacancies-actions'),
+    url(r'^edit_language_offsets/$', views.FSBidAdminProjectedVacancyEditLangOffsetsView.as_view(), name='admin-projected-vacancies-edit-language-offsets'),
+    url(r'^edit_capsule_description/$', views.FSBidAdminProjectedVacancyEditCapsuleDescView.as_view(), name='admin-projected-vacancies-edit-capsule-desc'),
 ]
 
 urlpatterns += router.urls
