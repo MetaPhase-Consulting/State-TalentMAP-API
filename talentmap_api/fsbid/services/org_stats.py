1d70ca6d6a42bd06175691abea7cdd8ffb900b89
musiqe31 <agbakwucharles@gmail.com>
2024-02-15 14:42:18 -0600

    updated key

--- talentmap_api/fsbid/services/org_stats.py
+++ talentmap_api/fsbid/services/org_stats.py
@@ -23,7 +23,7 @@ def org_stats_req_mapping(request):
         'pv_api_version_i': '',
         'pv_ad_id_i': '',
         'i_cycle_id': request.get('cycles') or 0,
-        'i_bureau_id': request.get('bureaus') or '',
+        'i_bureau_cd': request.get('bureaus') or '',
         'i_org_code': request.get('orgs') or '',
     }
 def org_stats_res_mapping(data):
