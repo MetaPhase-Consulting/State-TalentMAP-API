8f6571458b141f72223151aa1c327b9dc32975b8
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-21 09:22:49 -0500

    Added new urls for new endpoints

--- talentmap_api/fsbid/urls/admin_projected_vacancies.py
+++ talentmap_api/fsbid/urls/admin_projected_vacancies.py
@@ -10,8 +10,10 @@ urlpatterns = [
     url(r'^language_offsets/$', views.FSBidAdminProjectedVacancyLanguageOffsetsView.as_view(), name="admin-projected-vacancies-lang-offsets"),
     url(r'^$', views.FSBidAdminProjectedVacancyListView.as_view(), name="admin-projected-vacancies"),
     url(r'^edit/$', views.FSBidAdminProjectedVacancyActionsView.as_view(), name='admin-projected-vacancies-actions'),
-    url(r'^edit_language_offsets/$', views.FSBidAdminProjectedVacancyEditLangOffsetsView.as_view(), name='admin-projected-vacancies-edit-language-offsets'),
+    url(r'^edit_language_offsets/$', views.FSBidAdminProjectedVacancyEditLangOffsetsView.as_view(), name='admin-projected-vacancies-edit-lang-offsets'),
     url(r'^edit_capsule_description/$', views.FSBidAdminProjectedVacancyEditCapsuleDescView.as_view(), name='admin-projected-vacancies-edit-capsule-desc'),
+    url(r'^metadata/$', views.FSBidAdminProjectedVacancyMetadataView.as_view(), name='admin-projected-vacancies-metadata'),
+    url(r'^language_offsets/$', views.FSBidAdminProjectedVacancyMetadataView.as_view(), name='admin-projected-vacancies-lang-offsets'),
 ]
 
 urlpatterns += router.urls
