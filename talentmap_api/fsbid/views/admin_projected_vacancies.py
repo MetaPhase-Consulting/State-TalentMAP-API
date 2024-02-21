3c1f9d7f1f19ea3ec42ff26c62072b5a9ca0c657
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-02-21 09:23:20 -0500

    Added new endpoints, fixed permissions, allow array for edit pv request

--- talentmap_api/fsbid/views/admin_projected_vacancies.py
+++ talentmap_api/fsbid/views/admin_projected_vacancies.py
@@ -1,10 +1,10 @@
 import logging
 import coreapi
 
+from rest_condition import Or
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.views import APIView
 
 from drf_yasg.utils import swagger_auto_schema
@@ -19,7 +19,7 @@ class FSBidAdminProjectedVacancyFiltersView(APIView):
 
     # ======================== Get PV Filters ========================
 
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
 
     def get(self, request):
         '''
@@ -34,7 +34,7 @@ class FSBidAdminProjectedVacancyLanguageOffsetsView(APIView):
 
     # ======================== Get Language Offsets Dropdowns ========================
 
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
 
     def get(self, request):
         '''
@@ -49,7 +49,7 @@ class FSBidAdminProjectedVacancyListView(APIView):
 
     # ======================== Get PV List ========================
 
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
 
     @swagger_auto_schema(
         manual_parameters=[
@@ -75,30 +75,32 @@ class FSBidAdminProjectedVacancyActionsView(APIView):
     
     # ======================== Edit PV ========================
 
-    permission_classes = [IsAuthenticated, isDjangoGroupMember('superuser'), ]
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
 
     @swagger_auto_schema(request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'future_vacancy_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Projected Vacancy Seq Num'),
-            'future_vacancy_seq_num_ref': openapi.Schema(type=openapi.TYPE_STRING, description='Projected Vacancy Seq Num Reference'),
-            'positon_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Position Seq Num'),
-            'bid_season_code': openapi.Schema(type=openapi.TYPE_STRING, description='Bid Season Code'),
-            'assignment_seq_num_effective': openapi.Schema(type=openapi.TYPE_STRING, description='Assignment Seq Num Effective'),
-            'assignment_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Assignment Seq Num'),
-            'cycle_date_type_code': openapi.Schema(type=openapi.TYPE_STRING, description='Cycle Date Type Code'),
-            'future_vacancy_status_code': openapi.Schema(type=openapi.TYPE_STRING, description='Status Code'),
-            'future_vacancy_override_code': openapi.Schema(type=openapi.TYPE_STRING, description='Override Code'),
-            'future_vacancy_override_tour_end_date': openapi.Schema(type=openapi.TYPE_STRING, description='Override TED'),
-            'future_vacancy_system_indicator': openapi.Schema(type=openapi.TYPE_STRING, description='System Indicator'),
-            'future_vacancy_comment': openapi.Schema(type=openapi.TYPE_STRING, description='Comment'),
-            'created_date': openapi.Schema(type=openapi.TYPE_STRING, description='Created Date'),
-            'creator_id': openapi.Schema(type=openapi.TYPE_STRING, description='Creator ID'),
-            'updater_id': openapi.Schema(type=openapi.TYPE_STRING, description='Updater ID'),
-            'updated_date': openapi.Schema(type=openapi.TYPE_STRING, description='Updated Date'),
-            'future_vacancy_mc_indicator': openapi.Schema(type=openapi.TYPE_STRING, description='MC Indicator'),
-            'future_vacancy_exclude_import_indicator': openapi.Schema(type=openapi.TYPE_STRING, description='Exclude Import Indicator'),
-        }
+        type=openapi.TYPE_ARRAY, items=openapi.Items(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'future_vacancy_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Projected Vacancy Seq Num'),
+                'future_vacancy_seq_num_ref': openapi.Schema(type=openapi.TYPE_STRING, description='Projected Vacancy Seq Num Reference'),
+                'positon_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Position Seq Num'),
+                'bid_season_code': openapi.Schema(type=openapi.TYPE_STRING, description='Bid Season Code'),
+                'assignment_seq_num_effective': openapi.Schema(type=openapi.TYPE_STRING, description='Assignment Seq Num Effective'),
+                'assignment_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Assignment Seq Num'),
+                'cycle_date_type_code': openapi.Schema(type=openapi.TYPE_STRING, description='Cycle Date Type Code'),
+                'future_vacancy_status_code': openapi.Schema(type=openapi.TYPE_STRING, description='Status Code'),
+                'future_vacancy_override_code': openapi.Schema(type=openapi.TYPE_STRING, description='Override Code'),
+                'future_vacancy_override_tour_end_date': openapi.Schema(type=openapi.TYPE_STRING, description='Override TED'),
+                'future_vacancy_system_indicator': openapi.Schema(type=openapi.TYPE_STRING, description='System Indicator'),
+                'future_vacancy_comment': openapi.Schema(type=openapi.TYPE_STRING, description='Comment'),
+                'created_date': openapi.Schema(type=openapi.TYPE_STRING, description='Created Date'),
+                'creator_id': openapi.Schema(type=openapi.TYPE_STRING, description='Creator ID'),
+                'updater_id': openapi.Schema(type=openapi.TYPE_STRING, description='Updater ID'),
+                'updated_date': openapi.Schema(type=openapi.TYPE_STRING, description='Updated Date'),
+                'future_vacancy_mc_indicator': openapi.Schema(type=openapi.TYPE_STRING, description='MC Indicator'),
+                'future_vacancy_exclude_import_indicator': openapi.Schema(type=openapi.TYPE_STRING, description='Exclude Import Indicator'),
+            }
+        ), description='Projected Vacancy'
     ))
 
     def put(self, request):
@@ -115,7 +117,7 @@ class FSBidAdminProjectedVacancyEditLangOffsetsView(APIView):
     
     # ======================== Edit PV Language Offsets ========================
 
-    permission_classes = [IsAuthenticated, isDjangoGroupMember('superuser'), ]
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
 
     @swagger_auto_schema(request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
@@ -140,7 +142,7 @@ class FSBidAdminProjectedVacancyEditCapsuleDescView(APIView):
     
     # ======================== Edit PV Capsule Description ========================
 
-    permission_classes = [IsAuthenticated, isDjangoGroupMember('superuser'), ]
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
 
     @swagger_auto_schema(request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
@@ -161,3 +163,50 @@ class FSBidAdminProjectedVacancyEditCapsuleDescView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class FSBidAdminProjectedVacancyMetadataView(APIView):
+    
+    # ======================== Get PV Metadata ========================
+
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
+
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'future_vacancy_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Projected Vacancy Seq Num'),
+        }
+    ))
+
+    def put(self, request):
+        '''
+        Get Admin Projected Vacancy Metadata
+        '''
+        result = services.get_admin_projected_vacancy_metadata(request.data, request.META['HTTP_JWT'])
+        if result is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class FSBidAdminProjectedVacancyLangOffsetsView(APIView):
+    
+    # ======================== Get PV Language Offsets ========================
+
+    permission_classes = [IsAuthenticated, Or(isDjangoGroupMember('bureau_user'), isDjangoGroupMember('superuser'), ) ]
+
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={
+            'positon_seq_num': openapi.Schema(type=openapi.TYPE_STRING, description='Position Seq Num'),
+        }
+    ))
+
+    def put(self, request):
+        '''
+        Get Admin Projected Vacancy Language Offsets
+        '''
+        result = services.get_admin_projected_vacancy_lang_offsets(request.data, request.META['HTTP_JWT'])
+        if result is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
