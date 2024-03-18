ee2c9f50c2bb928381112b9068a5498f50d0512d
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-03-18 14:57:59 -0400

    Added URLs for each endpoint

---
+++ talentmap_api/fsbid/urls/cycle_job_categories.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from rest_framework import routers
+
+from talentmap_api.fsbid.views import cycle_job_categories as views
+
+router = routers.SimpleRouter()
+
+urlpatterns = [
+    url(r'^/$', views.FSBidCycleCategoriesView.as_view(), name="cycle-categories"),
+    url(r'^job_categories/$', views.FSBidCycleJobCategoriesView.as_view(), name="cycle-job-categories"),
+    url(r'^job_categories_edit$', views.FSBidCycleJobCategoriesActionView.as_view(), name="edit-cycle-job-categories"),
+]
+
+urlpatterns += router.urls
