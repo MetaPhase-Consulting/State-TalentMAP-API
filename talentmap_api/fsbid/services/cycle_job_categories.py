8b9874f591d04efffc4d88e116fbec0d66c9020b
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-03-18 18:31:45 -0400

    Debugging and renaming

--- talentmap_api/fsbid/services/cycle_job_categories.py
+++ talentmap_api/fsbid/services/cycle_job_categories.py
@@ -7,14 +7,14 @@ logger = logging.getLogger(__name__)
 
 # ======================== Get Cycle Categories ========================
 
-def get_cycle_categories(pk, jwt_token):
+def get_cycle_categories(jwt_token):
     '''
     Gets Cycle Categories
     '''
     args = {
         "proc_name": "qry_lstcyclejobs",
         "package_name": "PKG_WEBAPI_WRAP_SPRINT100",
-        "request_body": pk,
+        "request_body": {},
         "request_mapping_function": cycle_categories_req_mapping,
         "response_mapping_function": cycle_categories_res_mapping,
         "jwt_token": jwt_token,
@@ -41,14 +41,14 @@ def cycle_categories_res_mapping(response):
 
 # ======================== Get Cycle Job Categories ========================
 
-def get_cycle_job_categories(pk, jwt_token):
+def get_cycle_job_categories(jwt_token):
     '''
     Gets Cycle Job Categories for a Cycle Category
     '''
     args = {
         "proc_name": "qry_getcyclejob",
         "package_name": "PKG_WEBAPI_WRAP_SPRINT100",
-        "request_body": pk,
+        "request_body": {},
         "request_mapping_function": cycle_job_categories_req_mapping,
         "response_mapping_function": cycle_job_categories_res_mapping,
         "jwt_token": jwt_token,
@@ -61,7 +61,7 @@ def cycle_job_categories_req_mapping(request):
     return {
         "PV_API_VERSION_I": '',
         "PV_AD_ID_I": '',
-        "i_cc_cd": request,
+        "i_cc_cd": request.get('cycle_category_code'),
     }
 
 def cycle_job_categories_res_mapping(response):
@@ -100,8 +100,8 @@ def edit_cycle_job_categories_req_mapping(request):
         "PV_API_VERSION_I": "",
         "PV_AD_ID_I": "",
         "i_inclusion_ind": request.get('included'),
-        "i_cc_cd": request.get('cycle_code'),
-        "I_job_cat": request.get('job_codes'),
+        "i_cc_cd": request.get('cycle_category_code'),
+        "I_job_cat": request.get('job_category_codes'),
         "i_cjc_last_updt_tmsmp_dt": request.get('updated_dates'),
         "i_cjc_last_updt_user_id": request.get('updater_ids')
     }
