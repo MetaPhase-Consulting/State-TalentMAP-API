cd8a14580ad248992e8a4e76aad10a63849084d0
Jeanie Lam <jlam@metaphaseconsulting.com>
2024-07-12 10:38:42 -0400

    added post users to emp profile endpoint

--- talentmap_api/fsbid/views/employee.py
+++ talentmap_api/fsbid/views/employee.py
@@ -111,7 +111,7 @@ class FSBidAssignmentSeparationsBidsView(BaseView):
 
 
 class FSBidEmployeeProfileReportView(APIView):
-    permission_classes = [Or(isDjangoGroupMember('ao_user'), isDjangoGroupMember('cdo'))]
+    permission_classes = [Or(isDjangoGroupMember('ao_user'), isDjangoGroupMember('cdo'), isDjangoGroupMember('post_user'))]
 
     def get(self, request, pk):
         '''
