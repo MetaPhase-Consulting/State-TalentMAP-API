c17f7cdec61cc0f34865518bc1eab1ff7e7b87c3
musiqe31 <agbakwucharles@gmail.com>
2024-08-13 15:47:45 -0500

    testing new branch

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -60,21 +60,21 @@ def client(jwt_token, query, host=None):
 
     return response
 
-# def update_client(data, jwt_token, host=None):
-#     '''
-#     Update current client
-#     '''
-#     args = {
-#         "proc_name": 'prc_mod_alt_email_bscc',
-#         "package_name": 'Pkg_Wrap_dev',
-#         "request_mapping_function": update_client_req_mapping,
-#         "response_mapping_function": update_user_client_res_mapping,
-#         "jwt_token": jwt_token,
-#         "request_body": data,
-#     }
-#     return services.send_post_back_office(
-#         **args
-#     )
+def update_client(data, jwt_token, host=None):
+    '''
+    Update current client
+    '''
+    args = {
+        "proc_name": 'prc_mod_alt_email_bscc',
+        "package_name": 'Pkg_Wrap_dev',
+        "request_mapping_function": update_client_req_mapping,
+        "response_mapping_function": update_user_client_res_mapping,
+        "jwt_token": jwt_token,
+        "request_body": data,
+    }
+    return services.send_post_back_office(
+        **args
+    )
 
 def update_client_req_mapping(request):
     return {
