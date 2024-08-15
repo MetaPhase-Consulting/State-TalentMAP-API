f8d3ba8104277bcbc22e40b96270a8259904af56
musiqe31 <agbakwucharles@gmail.com>
2024-08-15 17:38:47 -0500

    updating type to int

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -12,7 +12,7 @@ import pydash
 from talentmap_api.fsbid.services import common as services
 import talentmap_api.fsbid.services.cdo as cdo_services
 import talentmap_api.fsbid.services.available_positions as services_ap
-from talentmap_api.common.common_helpers import combine_pp_grade, ensure_date, service_response
+from talentmap_api.common.common_helpers import combine_pp_grade, ensure_date
 from talentmap_api.fsbid.requests import requests
 
 
@@ -81,17 +81,21 @@ def update_client_req_mapping(request):
         "PV_AD_ID_I":"",
         "pv_subtran_i":0,
         "PV_WL_CODE_I":"",
-        "pv_hru_id_i": request.get("hru_id"),
-        "PV_PER_SEQ_NUM_I": request.get("per_seq_number"),
+        "pv_hru_id_i": int(request.get("hru_id")),
+        "PV_PER_SEQ_NUM_I": int(request.get("per_seq_number")),
         "PV_BSN_ID_I": request.get("bid_seasons"),
-        # for now this will not be used to add but will be needed later
+        # for now this will not be used, but will be needed later
         # "PV_BSCC_ID_I":null,
         "PV_BSCC_COMMENT_TEXT_I": request.get("comments"),
         "pv_cae_email_address_text_i": request.get("email"),
     }
     
 def update_user_client_res_mapping(data):
-    return service_response(data, 'Save User Client')
+    if data is None or (data['PV_RETURN_CODE_O'] != 0):
+        logger.error('FSBid call for Updating current client failed.')
+        return None
+
+    return data
 
 
 def get_clients_count(query, jwt_token, host=None):
