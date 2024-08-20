5b4ff8b120564c941a2876fe26292abd798b7967
musiqe31 <agbakwucharles@gmail.com>
2024-08-20 10:39:14 -0500

    added additional filters

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -77,7 +77,6 @@ def update_client(data, jwt_token, host=None):
     )
 
 def update_client_req_mapping(request):
-    print("REQUEST IN UPDATE CLIENT", request)
     return {
         "PV_AD_ID_I":"",
         "pv_subtran_i":0,
@@ -85,8 +84,8 @@ def update_client_req_mapping(request):
         "pv_hru_id_i": 12015,
         "PV_PER_SEQ_NUM_I": 6570,
         "PV_BSN_ID_I": 48,
-        "PV_BSCC_COMMENT_TEXT_I": "Test words",
-        "pv_cae_email_address_text_i": "test@test.com",
+        "PV_BSCC_COMMENT_TEXT_I": "Test aookes words",
+        "pv_cae_email_address_text_i": "testsss@test.com",
     }
 
 def update_user_client_res_mapping(data):
