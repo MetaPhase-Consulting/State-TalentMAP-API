6d6d79d374aad8ff495fe9aae8d0e869c9b62133
musiqe31 <agbakwucharles@gmail.com>
2024-08-26 15:45:23 -0500

    updated keys

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -81,11 +81,11 @@ def update_client_req_mapping(request):
         "PV_AD_ID_I":"",
         "pv_subtran_i":0,
         "PV_WL_CODE_I":"",
-        "pv_hru_id_i": request.get("hru_id"),
-        "PV_PER_SEQ_NUM_I": request.get("per_seq_number"),
-        "PV_BSN_ID_I": request.get("bid_seasons"),
-        "PV_BSCC_COMMENT_TEXT_I": request.get("comments"),
-        "pv_cae_email_address_text_i": request.get("email"),
+        "pv_hru_id_i": 12015,
+        "PV_PER_SEQ_NUM_I": 6570,
+        "PV_BSN_ID_I": "48, 54, 29",
+        "PV_BSCC_COMMENT_TEXT_I": "i",
+        "pv_cae_email_address_text_i": "i@i.com",
     }
 
 def update_user_client_res_mapping(data):
