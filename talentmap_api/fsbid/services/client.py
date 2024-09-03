26dd9cc3ad1d0a53671636a10931e7da49b59f2c
musiqe31 <agbakwucharles@gmail.com>
2024-09-03 18:11:43 -0500

    updating api

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -77,15 +77,13 @@ def update_client(data, jwt_token, host=None):
     )
 
 def update_client_req_mapping(request):
-    seasons = [request.get("bid_seasons")]
-    bidSeasons = ",".join([str(x) for x in seasons])
     return {
         "PV_AD_ID_I":"",
         "pv_subtran_i":0,
         "PV_WL_CODE_I":"",
         "pv_hru_id_i": request.get("hru_id"),
         "PV_PER_SEQ_NUM_I": request.get("per_seq_number"),
-        "PV_BSN_ID_I": bidSeasons,
+        "PV_BSN_ID_I": str(request.get("bid_seasons")),
         "PV_BSCC_COMMENT_TEXT_I": request.get("comments"),
         "pv_cae_email_address_text_i": request.get("email"),
     }
