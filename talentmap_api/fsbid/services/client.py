cc646e171b972d8f3b987e234b8cb1f1e857b210
musiqe31 <agbakwucharles@gmail.com>
2024-09-17 16:07:08 -0500

    testing bids

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -479,6 +479,8 @@ def convert_client_query(query, isCount=None):
     The TalentMap filters align with the client search filter naming
     '''
     from talentmap_api.fsbid.services.common import sorting_values, convert_multi_value
+    perdet_seq_nums = query.get("perdet_seq_num", [])
+    perdetNums = [str(x) for x in perdet_seq_nums] if perdet_seq_nums else None
     values = {
         "request_params.hru_id": hru_id_filter(query),
         "request_params.rl_cd": query.get("rl_cd", None),
@@ -495,7 +497,7 @@ def convert_client_query(query, isCount=None):
         "request_params.page_size": query.get("limit", 25),
         "request_params.currentAssignmentOnly": query.get("currentAssignmentOnly", 'true'),
         "request_params.get_count": query.get("getCount", 'false'),
-        "request_params.perdet_seq_num": query.get("perdet_seq_num", None),
+        "request_params.perdet_seq_num": perdetNums,
     }
     if isCount:
         values['request_params.page_size'] = None
