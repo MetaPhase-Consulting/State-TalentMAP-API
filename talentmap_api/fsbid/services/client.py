7d605f4758a879de1eb7db2e49d4e0b4e5ab619b
musiqe31 <agbakwucharles@gmail.com>
2024-09-18 10:55:26 -0500

    added comments and alt emails

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -321,6 +321,8 @@ def fsbid_clients_to_talentmap_clients(data):
 
     return {
         "id": str(employee.get("pert_external_id", None)),
+        "alt_email": pydash.get(data, 'alt_email'),
+        "comments": pydash.get(data, 'comments'),
         "hru_id": data.get("hru_id", None),
         "per_seq_num": employee.get("per_seq_num", None),
         "name": f"{employee.get('per_first_name', None)} {middle_name['full']}{employee.get('per_last_name', None)}{suffix_name}",
