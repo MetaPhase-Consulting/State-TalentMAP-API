07de946518003454d47dbe0e94f32395eed0f5ac
Felicia Imbriano <fimbriano@metaphaseconsulting.com>
2024-09-27 15:57:32 -0400

    add logger

--- talentmap_api/fsbid/services/common.py
+++ talentmap_api/fsbid/services/common.py
@@ -312,8 +312,10 @@ def send_post_back_office(proc_name, package_name, request_body, request_mapping
 
     try:
         logger.info(f"Sending back office call.....")
-        response = requests.post(url, headers={'JWTAuthorization': jwt_token, 'Content-Type': 'application/json'}, json=json_body).json()
+        response = requests.post(url, headers={'JWTAuthorization': jwt_token, 'Content-Type': 'application/json'}, json=json_body)
         logger.info(f"Back office call response: {response}")
+        response = response.json()
+        logger.info(f".json call: {response}")
     except:
         logger.error(f"FSBid backoffice call for procedure {proc_name} failed.")
         raise Exception('Error at FSBid call')
