f9cfcbeaf55767d83b380d93e9de0edd3ba14f47
Felicia Imbriano <fimbriano@metaphaseconsulting.com>
2024-09-30 14:50:28 -0400

    package typo

--- talentmap_api/fsbid/services/positions.py
+++ talentmap_api/fsbid/services/positions.py
@@ -514,7 +514,7 @@ def edit_el_positions(data, jwt_token):
 
     args = {
         "proc_name": "prc_iud_tracking_details_pos",
-        "package_name": "PKG_WEBAPI_WRAP_SPRINT",
+        "package_name": "PKG_WEBAPI_WRAP",
         "request_body": json_input,
         "request_mapping_function": None,
         "response_mapping_function": None,
