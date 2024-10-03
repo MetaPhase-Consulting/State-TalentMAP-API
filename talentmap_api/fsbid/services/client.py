0b81c48f33c006cd1bdce5ee52980b704022d7b7
musiqe31 <agbakwucharles@gmail.com>
2024-10-03 18:08:03 -0500

    updated API call

--- talentmap_api/fsbid/services/client.py
+++ talentmap_api/fsbid/services/client.py
@@ -118,10 +118,10 @@ def client_panel_update_req_mapping(request):
     }
 
 def client_panel_update_res_mapping(data):
-    if data is None and data['PV_RETURN_CODE_O'] is not 0:
-        logger.error('FSBid call for client perdets failed.')
-        return None
-    return [item['PER_SEQ_NUM1'] for item in data['PV_DETAIL_O']]
+    # if data is None and data['PV_RETURN_CODE_O'] is not 0:
+    #     logger.error('FSBid call for client perdets failed.')
+    #     return None
+    return data
 
 def convert_bidder_type_query(type):
     type_mapping = {
