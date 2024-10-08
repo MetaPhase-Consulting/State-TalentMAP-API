bdb8a1763d78c197bece83bd5e235b26fdb2b457
mansi-mp-200 <mshah@metaphaseconsulting.com>
2024-10-08 14:20:56 -0400

    adding more permissions to everything EL related

--- talentmap_api/fsbid/views/positions.py
+++ talentmap_api/fsbid/views/positions.py
@@ -63,7 +63,7 @@ class FSBidFrequentPositionsView(BaseView):
         return Response(services.get_frequent_positions(request.query_params, request.META['HTTP_JWT']))
 
 class FSBidEntryLevelPositionsView(BaseView):
-    permission_classes = (IsAuthenticatedOrReadOnly, isDjangoGroupMember('superuser'))
+    permission_classes = [IsAuthenticatedOrReadOnly, Or(isDjangoGroupMember('superuser'), isDjangoGroupMember('el_cdo'))]
 
     def get(self, request):
         """
@@ -72,7 +72,7 @@ class FSBidEntryLevelPositionsView(BaseView):
         return Response(services.get_el_positions(request.query_params, request.META['HTTP_JWT']))
 
 class FSBidEntryLevelPositionsFiltersView(BaseView):
-    permission_classes = (IsAuthenticatedOrReadOnly, isDjangoGroupMember('superuser'))
+    permission_classes = [IsAuthenticatedOrReadOnly, Or(isDjangoGroupMember('superuser'), isDjangoGroupMember('el_cdo'))]
 
     def get(self, request):
         """
